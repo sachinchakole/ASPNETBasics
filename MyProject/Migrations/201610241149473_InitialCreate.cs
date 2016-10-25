e523fa00a83a487fa6a29914097a180982f96873
sachinchakole <sachin@idpsolution.com.au>
2016-10-25 20:03:12 +0530

    Include Migration

---
+++ MyProject/Migrations/201610241149473_InitialCreate.cs
@@ -0,0 +1,44 @@
+namespace MyProject.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        CategoryId = c.Int(nullable: false, identity: true),
+                        CatName = c.String(),
+                    })
+                .PrimaryKey(t => t.CategoryId);
+            
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        ProductId = c.Int(nullable: false, identity: true),
+                        ProdName = c.String(),
+                        ProdPrice = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        ProdImageName = c.String(),
+                        ProdDescription = c.String(),
+                        CategoryId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.ProductId)
+                .ForeignKey("dbo.Categories", t => t.CategoryId, cascadeDelete: true)
+                .Index(t => t.CategoryId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Products", "CategoryId", "dbo.Categories");
+            DropIndex("dbo.Products", new[] { "CategoryId" });
+            DropTable("dbo.Products");
+            DropTable("dbo.Categories");
+        }
+    }
+}
